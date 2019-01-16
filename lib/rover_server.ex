f6dde6ccc04e07ff1844a1ccb6c5a8a8177ab137
diego santos <diego@nama.ai>
2019-01-16 00:36:22 -0200

    Refactoring RoverServer module and tests

--- lib/rover_server.ex
+++ lib/rover_server.ex
@@ -5,8 +5,12 @@ defmodule RoverOnMars.RoverServer do
   @name :rover_server
   @min_value_to_north_and_west 0
   @max_value_to_south_and_east 3
-  def start_link(init_state) do
-    GenServer.start_link(__MODULE__, %RoverState{current_position: init_state}, name: @name)
+  def start_link(init_direction, init_position) do
+    GenServer.start_link(
+      __MODULE__,
+      %RoverState{current_facing: init_direction, current_position: init_position},
+      name: @name
+    )
   end
 
   def init(state) do
@@ -81,6 +85,12 @@ defmodule RoverOnMars.RoverServer do
     {:reply, position, new_state}
   end
 
+  def handle_call(direction, _from, state) do
+    new_direction_facing = switch({direction, state.current_facing})
+    new_state = %{state | current_facing: new_direction_facing}
+    {:reply, new_direction_facing, new_state}
+  end
+
   def handle_info(:work, state) do
     {:noreply, state}
   end
@@ -94,15 +104,17 @@ defmodule RoverOnMars.RoverServer do
 
   ## Examples
 
-      iex> RoverOnMars.RoverServer.rotate_to({:left, :north})
+      iex> RoverOnMars.RoverServer.start_link(:north, [1,1])
+      iex> RoverOnMars.RoverServer.rotate_to(:left)
       :west
 
-      iex> RoverOnMars.RoverServer.rotate_to({:right, :north})
+      iex> RoverOnMars.RoverServer.start_link(:north, [1,1])
+      iex> RoverOnMars.RoverServer.rotate_to(:right)
       :east
 
   """
-  def rotate_to({direction, position}) do
-    switch({direction, position})
+  def rotate_to(direction) do
+    GenServer.call(@name, direction)
   end
 
   @doc """
@@ -110,11 +122,11 @@ defmodule RoverOnMars.RoverServer do
 
   ## Examples
 
-      iex> RoverOnMars.RoverServer.start_link([1,1])
+      iex> RoverOnMars.RoverServer.start_link(:north, [1,1])
       iex> RoverOnMars.RoverServer.move_to(:north)
       [0,1]
       
-      iex> RoverOnMars.RoverServer.start_link([0,0])
+      iex> RoverOnMars.RoverServer.start_link(:north, [0,0])
       iex> RoverOnMars.RoverServer.move_to(:north)
       [0,0]
 
--- test/rover_server_test.exs
+++ test/rover_server_test.exs
@@ -4,87 +4,86 @@ defmodule RoverServerTest do
 
   describe "When rotate to left" do
     test "Given the rover is facing North, when the rover rotates Left, then the rover is facing West" do
-      position = RoverServer.rotate_to({:left, :north})
+      position = get_rotate(:north, :left)
       assert :west == position
     end
 
     test "Given the rover is facing West, when the rover rotates Left, then the rover is facing South" do
-      position = RoverServer.rotate_to({:left, :west})
+      position = get_rotate(:west, :left)
       assert :south == position
     end
 
     test "Given the rover is facing South, when the rover rotates Left, then the rover is facing East" do
-      position = RoverServer.rotate_to({:left, :south})
+      position = get_rotate(:south, :left)
       assert :east == position
     end
 
     test "Given the rover is facing East, when the rover rotates Left, then the rover is facing North" do
-      position = RoverServer.rotate_to({:left, :east})
+      position = get_rotate(:east, :left)
       assert :north == position
     end
   end
 
   describe "When rotate to right" do
     test "Given the rover is facing North, when the rover rotates Right, then the rover is facing East" do
-      position = RoverServer.rotate_to({:right, :north})
+      position = get_rotate(:north, :right)
       assert :east == position
     end
 
     test "Given the rover is facing East, when the rover rotates Right, then the rover is facing South" do
-      position = RoverServer.rotate_to({:right, :east})
+      position = get_rotate(:east, :right)
       assert :south == position
     end
 
     test "Given the rover is facing South, when the rover rotates Right, then the rover is facing West" do
-      position = RoverServer.rotate_to({:right, :south})
+      position = get_rotate(:south, :right)
       assert :west == position
     end
 
     test "Given the rover is facing West, when the rover rotates Right, then the rover is facing North" do
-      position = RoverServer.rotate_to({:right, :west})
+      position = get_rotate(:west, :right)
       assert :north == position
     end
   end
 
   describe "When move forward" do
     test "Given the Is as position 1,1 and the rover is facing North, when the rover moves forward, the rover is in position 0,1" do
-      init_state = [0, 1]
-      {:ok, pid} = RoverServer.start_link(init_state)
-      current_position = RoverServer.move_to(:north)
-      Process.exit(pid, :kill)
-      assert init_state == current_position
+      current_position = get_position(:north, [0, 1])
+      assert [0, 1] == current_position
     end
 
     test "Given the Rover is facing West and is at position(0,0), when the user tries to move forward, the rovers position does not change" do
-      init_state = [0, 0]
-      {:ok, pid} = RoverServer.start_link(init_state)
-      current_position = RoverServer.move_to(:west)
-      Process.exit(pid, :kill)
-      assert init_state = current_position
+      current_position = get_position(:west, [0, 0])
+      assert [0, 0] = current_position
     end
 
     test "Given the Rover is facing North and is at position(0,0), when the user tries to move forward, the rovers position does not change" do
-      init_state = [0, 0]
-      {:ok, pid} = RoverServer.start_link(init_state)
-      current_position = RoverServer.move_to(:north)
-      Process.exit(pid, :kill)
-      assert init_state = current_position
+      current_position = get_position(:north, [0, 0])
+      assert [0, 0] = current_position
     end
 
     test "Given the Rover is facing South and is at position(3,0), when the user tries to move forward, the rovers position does not change" do
-      init_state = [3, 0]
-      {:ok, pid} = RoverServer.start_link(init_state)
-      current_position = RoverServer.move_to(:south)
-      Process.exit(pid, :kill)
-      assert init_state = current_position
+      current_position = get_position(:south, [3, 0])
+      assert [3, 0] = current_position
     end
 
     test "Given the Rover is facing East and is at position(0,3), when the user tries to move forward, the rovers position does not change" do
-      init_state = [0, 3]
-      {:ok, pid} = RoverServer.start_link(init_state)
-      current_position = RoverServer.move_to(:east)
+      current_position = get_position(:east, [0, 3])
+      assert [0, 3] = current_position
+    end
+
+    defp get_rotate(init_diretion, diretion_to_rotate) do
+      {:ok, pid} = RoverServer.start_link(init_diretion, [0, 0])
+      position = RoverServer.rotate_to(diretion_to_rotate)
       Process.exit(pid, :kill)
-      assert init_state = current_position
+      position
+    end
+
+    defp get_position(movimento_to, init_state) do
+      {:ok, pid} = RoverServer.start_link(:north, init_state)
+      current_position = RoverServer.move_to(movimento_to)
+      Process.exit(pid, :kill)
+      current_position
     end
   end
 end
